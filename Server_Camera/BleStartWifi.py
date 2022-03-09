cf30ae74828983f130dbaca1143717c2c86ecfec
Jean-Baptiste PERIN <jean-baptiste.perin@orange.com>
2022-03-09 16:41:46 +0100

    add doc and allow debug BLE handler

--- Server_Camera/BleStartWifi.py
+++ Server_Camera/BleStartWifi.py
@@ -4,10 +4,26 @@ import logging
 import asyncio
 import shlex, subprocess
 import pexpect.popen_spawn
-import local
+# import local
+import uuid
 
+DEBUG = True
+
+
+if DEBUG: import sys
 
 SCAN_WAIT_DURATION = 300
+
+# adresse MAC BLE du 
+BLE_MAC_ADR   = {
+    "e4:5f:01:0e:34:81" : "",
+    "e4:5f:01:0e:32:c2" : "24:0A:C4:C4:F5:26"}
+
+def retriveMacAdress():
+    return ':'.join(['{:02x}'.format((uuid.getnode() >> ele) & 0xff) for ele in range(0,8*6,8)][::-1])
+  
+blemacadr = BLE_MAC_ADR[retriveMacAdress()]
+print (blemacadr)
 
 async def bleMaintainLink(name, tab_event):
     logging.info (f"{name}--==< START >==--")
@@ -15,19 +31,23 @@ async def bleMaintainLink(name, tab_event):
     time.sleep(15)
     #child = pexpect.spawn(f'sudo bluetoothctl', encoding='utf-8')
     child = pexpect.popen_spawn.PopenSpawn(f'sudo bluetoothctl', encoding='utf-8')
+    if DEBUG: child.logfile_read = sys.stdout # foutread
+
     child.sendline("scan on")
     while(True):
-        idx = child.expect([f'Device {local.MAC_ADR} ServiceData Value:', pexpect.TIMEOUT], timeout=SCAN_WAIT_DURATION)
-        print('No detection BLE command = ',str(idx), ' (if 0 then rpi detect the ble command')
+        # idx = child.expect([f'Device {blemacadr} ServiceData Value:', pexpect.TIMEOUT], timeout=SCAN_WAIT_DURATION)
+        idx = child.expect([f'Device {blemacadr} ServiceData ', pexpect.TIMEOUT], timeout=SCAN_WAIT_DURATION)
         if idx == 0 : 
             #process = subprocess.run(shlex.split(f'sudo ifdown wlan0'))
-            logging.info (f"{name} stating wifi")
+            logging.info (f"{name} starting wifi")
             subprocess.call(["rfkill", "unblock", "wifi"])
             time.sleep(1)
-            logging.info (f"{name} launching server")
-            subprocess.call(["node", local.JSERVER_PATH, "&"])
+            # logging.info (f"{name} launching server")
+            # subprocess.call(["node", local.JSERVER_PATH, "&"])
             #subprocess.call(["sudo", "ps", "-a"])
             #print('Trouvé ' + str(idx))
+        else:
+            print('No detection BLE command = ',str(idx), ' (if 0 then rpi detect the ble command')
         
 
 async def main():
--- Server_Camera/README.md
+++ Server_Camera/README.md
@@ -14,3 +14,34 @@ node server2.js
 
 Pour forcer l'usage du wifi
 `sudo ifconfig eth0 down`
+
+## Commandes utiles
+
+Lister l'état des interface radio
+
+`rfkill list`
+
+
+
+### Wifi 
+
+Lancer le client wifi
+
+`wpa_supplicant -B -c/etc/wpa_supplicant/wpa_supplicant.conf -iwlan0 -Dnl80211,wext`
+
+
+### Bluetooth
+
+Connaître l'état du service bluetooth:
+`sudo service bluetooth status`
+
+Utiliser le bluetooth en ligne de commande
+```
+sudo bluetoothctl
+power on
+scan on
+```
+
+Connaître l'état de l'interface Bluetooth
+`sudo hciconfig hci0`
+
