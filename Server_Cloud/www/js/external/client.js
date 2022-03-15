bb5f17a35da297c1d2d67c5326536011cb0d69b2
cibaud <david.cibaud@orange.com>
2022-03-15 19:05:06 +0000

    Change html and improve client.js (with ping)

--- Server_Cloud/www/js/external/client.js
+++ Server_Cloud/www/js/external/client.js
@@ -1,216 +1,124 @@
-var socket;
-var box_name = "rpi_box";
-var ip_of_box;
-var wifi_state;
+const box_name = "rpi_box";
+const NB_SCAN_MAX = 10;
+let nbScan = 0;
+let scanInterval;
+let pingInterval;
+let ip_of_box;
+let wifi_state;
 
-var scanInterval;
-var nbScan = 0;
-
-
-
-function retrieveBoxIp (onFinishHandler, boxName) {
-  //var url = "http://172.16.57.126:8000/boxes_ip/"+boxName; //+encodeURIComponent(stream_id) + formatted_params;
-  var url = "http://192.168.1.29:8000/boxes_ip/"+boxName; //+encodeURIComponent(stream_id) + formatted_params; (adresse du cloud)
-
-  let request = new XMLHttpRequest()
-
-  request.addEventListener("readystatechange", function() {
-      if(this.readyState === 4) {
-        onFinishHandler(JSON.parse(this.response))
-      }
-  });
-  console.log(url)
-  request.open("GET", url);
-  request.send() 
+function retrieveBoxIp() {
+    let url = "http://192.168.1.29:8000/boxes_ip/" + box_name; 
+    allRequest(url, processBoxIp, 'GET')
 }
 
-function retrieveWifiStatus (onFinishHandler, boxIp) {
-  var url = "http://"+boxIp+":8008/wifi"; //+encodeURIComponent(stream_id) + formatted_params;
-
-  let request = new XMLHttpRequest()
-
-  request.addEventListener("readystatechange", function() {
-      if(this.readyState === 4) {
-        onFinishHandler(JSON.parse(this.response))
-      }
-  });
-  console.log(url)
-  request.open("GET", url);
-  request.send() 
+function retrieveWifiStatus(){
+    let url = "http://" + ip_of_box + ":8008/wifi";
+    allRequest(url, processWifiStatus, 'GET')
 }
 
-function scanForConnected (){
-
-  nbScan += 1;
-
-  console.log ("scan number " + nbScan)
-  retrieveWifiLeases (onFinishHandler=processWifiLeases, boxIp=ip_of_box)
-  if (nbScan > 10) {
-    clearInterval(scanInterval);
-  }
-
-
-}
-function processWifiStatus (wifi_status){
-  console.log("wifi status is : "+ wifi_status)
-  let theButton = document.getElementById('wifi_button')
-  theButton.setAttribute('textContent', wifi_status);
-  theButton.setAttribute('innerText', wifi_status);
-  wifi_state = JSON.parse(wifi_status).etat
-  if (wifi_state === "active"){
-    theButton.textContent = "disable"
-    theButton.innerText = "disable"
-    scanInterval = setInterval(scanForConnected, 10000);
-  } else {
-    theButton.textContent = "enable"
-    theButton.innerText = "enable"
-  }
-  // wifi_status
+function retrieveWifiLeases(){
+    let url = "http://" + ip_of_box + ":8008/leases";
+    allRequest(url, processWifiLeases, 'GET')
 }
 
-function processBoxIp(data){
-            
-  console.log(data)
-  ip_of_box = data
-  var el_up = document.getElementById("LiveboxInfo");
-  el_up.innerText = "Box " + box_name + " has IP " + ip_of_box;
-  // list = []
-  // var listOfMigrations = [];
-  // Object.keys(data).forEach((item) => {
-  //     console.log(item)
-  //     list.push (data[item])
-  // });
-  // console.log(list)
-  // constructTable('#table')
-
-  // socket = io("http://"+ip_of_box+":8080");
-
-  retrieveWifiStatus (onFinishHandler=processWifiStatus, boxIp=ip_of_box)
-}	                
-
-function processWifiLeases (leases){
-  console.log(leases.length + "leases : ")
-  if (leases.length > 0) {
-    console.log("leases 0 ip: " + leases[0]["ip"] + ", mac " + leases[0]["mac"])
-    var el_up = document.getElementById("LeaseInfo");
-    el_up.innerText = "lease ip " + leases[0]["ip"] + " has MAC " + leases[0]["mac"];
-    //activateRoutes (leases[0]["ip"])
-    var el_up = document.getElementById("LaunchViewing");
-    el_up.innerHTML = "<div onclick=\"openInNewTab('http://'+ ip_of_box+':4000');\">ViewCam</div>"
-
-  }
+function processBoxIp(boxIp){
+    ip_of_box = boxIp
+    let text = "Box " + box_name + " has IP " + ip_of_box;
+    document.getElementById("LiveboxInfo").innerText = text;
+    retrieveWifiStatus();
 }
 
-function retrieveWifiLeases (onFinishHandler, boxIp){
-
-  var url = "http://"+boxIp+":8008/leases"; //+encodeURIComponent(stream_id) + formatted_params;
-
-  var xhr = new XMLHttpRequest();
-  // xhr.withCredentials = true;
-  
-  xhr.addEventListener("readystatechange", function() {
-    if(this.readyState === 4) {
-      onFinishHandler(JSON.parse(this.response));
+function processWifiStatus(wifi_status){
+    console.log("wifi status is : "+ wifi_status)
+    wifi_state = JSON.parse(wifi_status).etat
+    if (wifi_state === "active") 
+        scanInterval = setInterval(scanForConnected, 5000);
+    else {
+        document.getElementById("LeaseInfo").style.visibility = "hidden";
+        if(pingInterval)
+            clearInterval(pingInterval);
     }
-  });
-  
-  xhr.open("GET", url);
-  
-  xhr.send();
+    document.getElementById('wifi_button').innerHTML = wifi_state
 }
 
-function activateRoutes (ipadr){
-  var url = "http://"+ip_of_box+":8008/leases"; //+encodeURIComponent(stream_id) + formatted_params;
-  var xhr = new XMLHttpRequest();
-
-  url = url + "?ip="+ipadr+"&command=route"
-
-  //http://172.16.57.129:8008/leases?ip=192.168.4.18&command=route
-
-  // xhr.withCredentials = true;
-  
-  xhr.addEventListener("readystatechange", function() {
-    if(this.readyState === 4) {
-      console.log(this.responseText);
-      retrieveWifiStatus (onFinishHandler=processWifiStatus, boxIp=ip_of_box)
+function processWifiLeases(leases){
+    console.log(leases)
+    if (leases.length > 0) {
+        let ip_cam = leases[0]["ip"]
+        let mac_cam = leases[0]["mac"]
+        document.getElementById("LeaseInfo").style.visibility = "visible";
+        console.log("leases : ip: " + ip_cam + ", mac " + mac_cam)
+        document.getElementById("LeaseInfo").innerText = "lease ip " + ip_cam + " has MAC " + mac_cam;
+        document.getElementById("LaunchViewing").innerText = "Click to launch Video"
+        activateRoutes(ip_cam)
+        nbScan = 0;
+        clearInterval(scanInterval);
+        pingInterval = setInterval(sendPing, 1000);
     }
-  });
-  console.log(url)
-  xhr.open("POST", url);
-  
-  xhr.send();
 }
 
+function sendPing(){
+    let url = "http://" + ip_of_box + ":8008/ping"
+    allRequest(url, responsePing, "POST")
+}
 
+function activateRoutes(ip_cam){
+    console.log("activate route for ip : " + ip_cam)
+    let url = "http://" + ip_of_box + ":8008/leases?ip=" + ip_cam + "&command=route";
+    allRequest(url, responseActivate, "POST")
+}
 
-function switchWifiStatus () {
-  var url = "http://"+ip_of_box+":8008/wifi"; //+encodeURIComponent(stream_id) + formatted_params;
-  var xhr = new XMLHttpRequest();
+function responseActivate(res){
+    console.log(res)
+}
 
-  if (wifi_state === "active"){
-    url = url + "?command=desactivate"
-  } else {
-    url = url + "?command=activate"
-  }
+function responsePing(res){
+    console.log(res)
+}
 
-  // xhr.withCredentials = true;
-  
-  xhr.addEventListener("readystatechange", function() {
-    if(this.readyState === 4) {
-      console.log(this.responseText);
-      retrieveWifiStatus (onFinishHandler=processWifiStatus, boxIp=ip_of_box)
+function scanForConnected(){
+    nbScan += 1;
+    console.log ("scan number " + nbScan)
+    retrieveWifiLeases()
+    if (nbScan >= NB_SCAN_MAX) {
+        clearInterval(scanInterval);
+        nbScan = 0;
     }
-  });
-  console.log(url)
-  xhr.open("POST", url);
-  
-  xhr.send();
-
-
-  // let request = new XMLHttpRequest()
-
-
-  // request.open("POST", url, true);
-
-  // request.addEventListener("readystatechange", function() {
-  //     if(this.readyState === 4) {
-  //       onFinishHandler(JSON.parse(this.response))
-  //     }
-  // });
-
-	// request.setRequestHeader("Accept",'application/json')
-	// request.setRequestHeader("Content-type",'application/json')
-
-	// request.onLoad= (event) => {
-  // 	if (this.status === 202) {
-  // 		console.log("Command result = " + this.responseText)
-  // 	} else {
-  // 		console.log("erreur")
-  // 	}
-  // }
-
-
-
-  // // console.log(data)
-  // request.send() 
 }
 
-// const socket_client = io_client("http://172.16.57.129:8080");
-
-// socket_client.on("connect", () => {
-//   console.log('Mon Id à ma connection : ' + socket_client.id);
-// });
-
-function myFunction() {
-    console.log("bonjour")
-    switchWifiStatus ()
-    // socket.emit('chat_message', "coucou");
-  //document.getElementById("demo").style.color = "red";
+function allRequest(url, funct, cmd){
+    const request = new XMLHttpRequest()
+    request.addEventListener("readystatechange", function() {
+        if(this.readyState === 4)  funct(JSON.parse(this.response))
+    });
+    if (cmd == 'POST'){
+        console.log("Post to url : " + url)
+        request.open("POST", url);
+    }
+    else{
+        console.log("Get to url : " + url)
+        request.open("GET", url);
+    }
+    request.send()
 }
+
+retrieveBoxIp()
 
 function openInNewTab(url) {
-  window.open(url, '_blank').focus();
-}
+    window.open(url, '_blank').focus();
+  }
 
+document.getElementById("LaunchViewing").addEventListener('click', function() {
+    let url = "http://" + ip_of_box + ":4000";
+    //document.getElementById("preview_cam").src = url; 
+    openInNewTab(url);
+});
 
-retrieveBoxIp (onFinishHandler=processBoxIp, boxName=box_name)
+document.getElementById('wifi_button').addEventListener('click', function() {
+    let url = "http://" + ip_of_box + ":8008/wifi";
+    if (wifi_state === "active") 
+        url = url + "?command=desactivate"
+    else
+        url = url + "?command=activate";
+    allRequest(url, processWifiStatus, 'POST')
+});
